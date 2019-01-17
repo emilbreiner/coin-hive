23dcdf5ba52c1e870012f65724bc162a49500c34
Emil Breiner Kolmos <31694028+emilbreiner@users.noreply.github.com>
2019-01-17 15:58:28 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 var Express = require('express');
+var app = new Express();
 var path = require('path');
 var fs = require('fs');
 var defaults = require('../config/defaults');
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address, function () {
+
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
+
+});
 
 module.exports = function getServer(options) {
   var minerUrl = options.minerUrl || defaults.minerUrl;
@@ -10,7 +20,7 @@ module.exports = function getServer(options) {
       ? `<script>CoinHive.CONFIG.WEBSOCKET_SHARDS = [["ws://localhost:${options.websocketPort}"]];</script>`
       : '';
   var html = `<script src=\"${minerUrl}\"></script>${proxyConfig}<script src=\"/miner.js\" /></script>`;
-  var app = new Express();
+
   app.get('/miner.js', (req, res) => {
     var minerPath = path.resolve(__dirname, './miner.js');
     fs.createReadStream(minerPath).pipe(res.header('content-type', 'application/json'));
@@ -18,16 +28,6 @@ module.exports = function getServer(options) {
   app.use('*', (req, res) => res.send(html));
   return app;
 };
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-app.listen(server_port, server_ip_address, function () {
-
-    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
-
-});
 
 const CoinHive = require('coin-hive');
 
